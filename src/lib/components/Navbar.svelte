8da44dcc2f05eb5024c5e574c3dff5f2b55494ec
Wilfredo Mejia Palma <wilfredomejiapalma@gmail.com>
2024-09-19 22:37:45 -0600

    Added icon library and used it to make dark mode switch

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { Sun, Moon } from "lucide-svelte";
     import logo from "$lib/assets/logo.svg";
     import Button from "./ui/button/button.svelte";
     import { toggleMode } from "mode-watcher";
@@ -7,7 +8,7 @@
     export let loggedIn;
 </script>
 
-<div class="navbar p-2 bg-muted outline outline-muted-foreground">
+<div class="navbar pt-3 bg-muted outline outline-muted-foreground">
     <div class="content">
         <div style="display: flex; width: 100%">
             <img src={logo} alt="Logo" class="logo" />
@@ -25,19 +26,15 @@
                     <a href="auth/login/twitch"> login </a>
                 {/if}
             </div>
+            <Button class="pb-3" on:click={toggleMode} variant="" size="icon">
+                <Sun
+                class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+              />
+              <Moon
+                class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+              />
+            </Button>
         </div>
-        <Button class="max-h-" on:click={toggleMode} variant="outline" size="icon">
-            <p
-            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-          >
-          L
-          </p>
-          <p
-            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-          >
-          D
-          </p>
-        </Button>
     </div>
 </div>
 
@@ -50,18 +47,12 @@
         top: 0;
     }
     .content{
-        margin-left: auto;
-        margin-right: auto;
         display: flex;
     }
     .links {
         display: flex;
         gap: 1rem;
         margin-left: auto;
-    }
-    .dropdown {
-        margin-left: auto;
-        display: none;
     }
 	
   
